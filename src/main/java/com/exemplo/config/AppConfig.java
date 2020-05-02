57c3ae0fd8d2fe61240db35a1c00d47e3afbdab6
Wagmar Queiroga <wagmar.queiroga@riocard.com>
2020-05-02 15:03:08 -0300

    add redirecionamento para doc da api

--- src/main/java/com/exemplo/config/AppConfig.java
+++ src/main/java/com/exemplo/config/AppConfig.java
@@ -4,14 +4,28 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
 
     @Bean
     public RestTemplate restTemplate() {
         SimpleClientHttpRequestFactory httpRequestFactory = new SimpleClientHttpRequestFactory();
         httpRequestFactory.setReadTimeout(10000);
         return new RestTemplate(httpRequestFactory);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/docs/**").addResourceLocations("classpath:/static/docs/index.html");
+    }
+
+    @Override
+    public void addViewControllers (ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/index.html", "/static/docs/index.html");
+        registry.addRedirectViewController("/", "/static/docs/index.html");
     }
 }
