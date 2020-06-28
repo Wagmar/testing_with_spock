8850c7d6de6f3d0c99b0686cf613e7a424d0846c
Wagmar Queiroga <wagmar.queiroga@riocard.com>
2020-06-28 17:57:22 -0300

    add conteroler e checagem se foi injetado.

--- src/test/java/com/exemplo/ExemploApplicationTests.java
+++ src/test/java/com/exemplo/ExemploApplicationTests.java
@@ -1,16 +1,22 @@
 package com.exemplo;
 
+import com.exemplo.api.v1.ExemploRestController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ExemploApplicationTests {
 
+	@Autowired
+	private ExemploRestController controller;
+
 	@Test
 	public void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
-
 }
